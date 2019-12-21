801eb72e2419ebf3533deec4e120fd02676f401e
Neal JIn Oh <no4259@gmail.com>
2019-12-21 01:45:34 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,17 @@
+let h1 = document.createElement('h1')
+h1.setAttribute('id', 'victory')
+
+
+
+
+// it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+//       expect(newHeader.nodeName, "Make sure you create an <h1> with id 'victory'").eql('H1')
+//     });
+
+//     it("has a 'newHeader' variable that points to node 'h1#victory'", () => {
+//       expect(newHeader.id, "Make sure you create an <h1> with id 'victory'").eql('victory')
+//     });
+
+//     it("has a 'newHeader' variable that points to node 'h1#victory' with \"YOUR-NAME is the champion\" inside", () => {
+//       expect(newHeader.innerHTML, "Make sure you create an <h1> with id 'victory' with a sweet message in it").to.include("is the champion");
+//     });
